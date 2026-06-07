7eed3e48310e26cbdda234969f99c7b4b0f836aa
keerthijonnalagadda27-star <keerthijonnalagadda27@gmail.com>
2026-06-07 12:41:25 +0530

    fixed host problem

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const api=axios.create({
-    baseURL: 'http://localhost:8000',
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
 })
 //Interceptor means:
 // Code that automatically runs before or after every request. 
