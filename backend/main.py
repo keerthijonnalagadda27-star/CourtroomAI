8d8139f817848c3a99e5305c4dcd0cfd384aa10f
keerthijonnalagadda27-star <keerthijonnalagadda27@gmail.com>
2026-06-07 12:52:20 +0530

    fix CORS

--- backend/main.py
+++ backend/main.py
@@ -57,7 +57,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://courtroomai.vercel.app"],
+    "https://courtroomai.vercel.app",
+    "https://courtroomai-m66stzkgk-sree-keerthi-s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
